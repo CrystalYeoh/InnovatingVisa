402585b43b7240cdb9c7b1f863da78e34a2da59e
silentfatez <keith_goh@silentfatez.com>
2020-06-24 13:10:29 +0800

    update

---
+++ innovatingvisa/src/components/PageComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from "react-router-dom";
+
+function PageShower(props) {
+  return (
+    <div className="container">
+      <h4>Page</h4>
+    </div>
+  );
+}
+
+export default PageShower;
