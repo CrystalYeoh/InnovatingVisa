0f07b131f815acda2ed5aa8116729a0cbf6887a2
CrystalYeoh <crystalyeohje@gmail.com>
2020-06-29 16:19:38 +0800

    Added backend component from Keith + merchantSignUp posting

---
+++ backend/test.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+
+def sql_GCP_Insert(sqlstring):
+    engine = create_engine('mysql+pymysql://root:Visa1234@34.87.113.249:3306/testDB')
+    with engine.connect() as con:
+
+        rs = con.execute(sqlstring)
+        print(list(rs))
+
+
+# sql_GCP("""CREATE TABLE Persons (
+#     PersonID int,
+#     LastName varchar(255),
+#     FirstName varchar(255),
+#     Address varchar(255),
+#     City varchar(255)); """)
+sql_GCP("""SELECT * FROM STATION;""")
