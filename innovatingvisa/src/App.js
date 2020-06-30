f6c26395bd2cb4cdf6f9628420baa6f23a111ee8
KR Chandra <anicd01@gmail.com>
2020-06-30 22:43:13 +0530

    Added js scripts in index.html and defined LoginComponenet

--- innovatingvisa/src/App.js
+++ innovatingvisa/src/App.js
@@ -5,19 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ConfigureStore } from "./redux/configureStore";
 import "./App.css";
+import Login from "./components/LoginComponentTest";
 
 const store = ConfigureStore();
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div className="App">
-            <Main />
-          </div>
-        </BrowserRouter>
-      </Provider>
+      <Login />
     );
   }
 }
---
+++ innovatingvisa/src/components/LoginComponentTest.js
@@ -0,0 +1,32 @@
+import React,{Component} from 'react';
+import {Media, Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
+
+
+
+
+class Login extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    
+    // onVisaCheckoutReady() {
+        
+
+    // window.V.init( {
+    //         apikey: "H2F1875VVBF25Q6D97E2217IQtcmIXPnlA5Xi-2jKBSLQbUzE",
+    //         // encryptionKey: "Z8SZ5DJE12EH7VS966HG13Am9GNwjEvKgRNwY11qu0Nf2ZtaA",
+    //         paymentRequest: { currencyCode: "USD",  subtotal: "10.00"}});
+    // window.V.on("payment.success", function(payment) {document.write("payment.success: \n" + JSON.stringify(payment)); });
+    // window.V.on("payment.cancel", function(payment) { document.write("payment.cancel: \n" + JSON.stringify(payment)); });
+    // window.V.on("payment.error", function(payment, error) { document.write("payment.error: \n" +  JSON.stringify(payment) + "\n" + JSON.stringify(error));});
+    // };
+    
+    render(){
+        return (
+            <CardImg width="100%" className="v-button" src={"https://sandbox.secure.checkout.visa.com/wallet-services-web/xo/button.png"}  alt={"Visa Checkout"} />
+        );
+        }
+}
+
+export default Login;
