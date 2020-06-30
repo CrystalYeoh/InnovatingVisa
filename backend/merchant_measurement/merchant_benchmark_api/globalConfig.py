54f1f664821548be26a6f6b8aec9f4fbe30d6b62
CrystalYeoh <crystalyeohje@gmail.com>
2020-06-30 17:44:45 +0800

    Trying w the merchant api

--- backend/merchant_measurement/merchant_benchmark_api/globalConfig.py
+++ backend/merchant_measurement/merchant_benchmark_api/globalConfig.py
@@ -4,10 +4,10 @@ class GlobalConfig:
 		self.present = True
 		self.userName = 'N2OJZRQQFD6BTH5VJOK721dvADC3Oqj3RJBr4SFQpP9PB2ULo'
 		self.password = 'tzmw7eQS26mr8'
-		self.certificatePath = '/absolute/path/to/cert.pem'
-		self.privateKeyPath = '/absolute/path/to/key_90012af2-1e19-491c-b9e6-6aba4341f37c.pem'
-		self.caCertPath = '/absolute/path/to/DigiCertGlobalRootCA.crt'
-		self.proxyUrl = 'PROXY_URL'
+		self.certificatePath = '/merchant_measurement/merchant_benchmark_api/certs/cert.pem'
+		self.privateKeyPath = '/merchant_measurement/merchant_benchmark_api/certs/key_90012af2-1e19-491c-b9e6-6aba4341f37c.pem'
+		self.caCertPath = '/merchant_measurement/merchant_benchmark_api/certs/DigiCertGlobalRootCA.crt'
+		self.proxyUrl = "https://sandbox.api.visa.com"
 		self.sharedSecret = 'YOUR_SHARED_SECRET'
 		self.apiKey = 'YOUR_APIKEY'
 		self.mleKeyId = 'YOUR_MLE_KEY_ID'
--- backend/merchant_measurement/merchant_benchmark_api/test/test_merchant_benchmark_api.py
+++ backend/merchant_measurement/merchant_benchmark_api/test/test_merchant_benchmark_api.py
@@ -25,6 +25,8 @@ import json
 
 from src.apis.merchant_benchmark_api import MerchantBenchmarkApi
 from src.configuration import Configuration
+
+sys.path.append('../')
 from globalConfig import GlobalConfig
 
 
