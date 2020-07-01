e7a9997d33f12318dde22703d8697442a67ff66f
silentfatez <41805687+silentfatez@users.noreply.github.com>
2020-07-02 00:38:13 +0800

    ga

--- innovatingvisa/src/App.js
+++ innovatingvisa/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import Main from "./components/MainComponent";
 import { BrowserRouter } from "react-router-dom";
-
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 import "./App.css";
 import "./components/routes/store.css";
+const history = createBrowserHistory();
 
 class App extends Component {
   render() {
+    ReactGA.initialize("UA-171419667-1");
+
+    // Initialize google analytics page view tracking
+    history.listen((location) => {
+      ReactGA.set({ page: location.pathname }); // Update the user's current page
+      ReactGA.pageview(location.pathname); // Record a pageview for the given page
+    });
     return (
-      <BrowserRouter>
+      <BrowserRouter history={history}>
         <div className="App">
           <Main />
         </div>
